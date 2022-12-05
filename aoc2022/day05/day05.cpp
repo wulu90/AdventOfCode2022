507100a789c79a8722491b9f66efd748284536ab
wulu <wulu90@outlook.com>
2022-12-05 15:54:03 +0800

    day05 add files

---
+++ aoc2022/day05/day05.cpp
@@ -0,0 +1,58 @@
+#include <deque>
+#include <fstream>
+#include <iostream>
+#include <stack>
+#include <string>
+#include <vector>
+
+#include <cstdio>
+
+void build_stack(std::string& line, std::vector<std::deque<char>>& stacks) {
+    size_t pos = 0;
+    while (pos < line.size()) {
+        size_t inx = line.find('[', pos);
+
+        stacks[inx / 4].push_back(line[inx + 1]);    // pattern like  "[A] " ,so divide by 4;
+
+        pos += 3;
+    }
+}
+
+int main() {
+    std::ifstream input("input");
+    std::string line;
+
+    std::vector<std::deque<char>> stacks;
+    std::deque<char> ss;
+    std::getline(input, line);
+
+    int stack_size = (line.size() + 1) / 4;
+    for (int i = 0; i < 9; i++) {
+        stacks.push_back(ss);
+    }
+    build_stack(line, stacks);
+
+    while (std::getline(input, line)) {
+        if (line[1] == '1')
+            break;
+
+        build_stack(line, stacks);
+    }
+
+    std::getline(input, line);    // read blank line;
+
+    while (std::getline(input, line)) {
+        int crate_count, stack_from, stack_to;
+        std::sscanf(line.c_str(), "move %d from %d to %d", &crate_count, &stack_from, &stack_to);
+
+        for (int i = 0; i < crate_count; i++) {
+            stacks[stack_to].push_front(stacks[stack_from].front());
+            stacks[stack_from].pop_front();
+        }
+    }
+
+    for (size_t i = 0; i < stacks.size(); i++) {
+        std::cout << stacks[i].front();
+    }
+    std::cout << std::endl;
+}
