0e765eb986cbd209d8e25be7f6fda5942cf8bd10
wulu <wulu90@outlook.com>
2022-12-09 17:56:56 +0800

    day09 part2

--- aoc2022/day09/day09.cpp
+++ aoc2022/day09/day09.cpp
@@ -1,3 +1,4 @@
+#include <array>
 #include <fstream>
 #include <functional>
 #include <iostream>
@@ -24,27 +25,50 @@ void check_touching_and_move(std::pair<int, int>& head, std::pair<int, int>& tai
     }
 }
 
-void right(std::pair<int, int>& head, std::pair<int, int>& tail) {
+bool check_touching_and_moving(std::pair<int, int>& head, std::pair<int, int>& tail) {
+    if (head != tail) {
+        if (head.first == tail.first)    // same row
+        {
+            if (std::abs(head.second - tail.second) > 1) {
+                tail.second += head.second > tail.second ? 1 : -1;
+                return false;
+            }
+            return true;
+        } else if (head.second == tail.second) {
+            if (std::abs(head.first - tail.first) > 1) {
+                tail.first += head.first > tail.first ? 1 : -1;
+                return false;
+            }
+            return true;
+        } else {
+            if (!(std::abs(head.first - tail.first) == 1 && std::abs(head.second - tail.second) == 1)) {
+                tail.first += head.first > tail.first ? 1 : -1;
+                tail.second += head.second > tail.second ? 1 : -1;
+                return false;
+            }
+            return true;
+        }
+    }
+    return true;
+}
+
+void right(std::pair<int, int>& head) {
     head.second += 1;
-    check_touching_and_move(head, tail);
 }
 
-void left(std::pair<int, int>& head, std::pair<int, int>& tail) {
+void left(std::pair<int, int>& head) {
     head.second -= 1;
-    check_touching_and_move(head, tail);
 }
 
-void up(std::pair<int, int>& head, std::pair<int, int>& tail) {
+void up(std::pair<int, int>& head) {
     head.first += 1;
-    check_touching_and_move(head, tail);
 }
 
-void down(std::pair<int, int>& head, std::pair<int, int>& tail) {
+void down(std::pair<int, int>& head) {
     head.first -= 1;
-    check_touching_and_move(head, tail);
 }
 
-std::function<void(std::pair<int, int>& head, std::pair<int, int>& tail)> move_func(char d) {
+std::function<void(std::pair<int, int>& head)> move_func(char d) {
     if (d == 'R')
         return right;
     else if (d == 'L')
@@ -58,12 +82,26 @@ std::function<void(std::pair<int, int>& head, std::pair<int, int>& tail)> move_f
 void move(char d, int step, std::pair<int, int>& head, std::pair<int, int>& tail, std::set<std::pair<int, int>>& tailvisited) {
     auto f = move_func(d);
     while (step--) {
-        f(head, tail);
+        f(head);
+        check_touching_and_move(head, tail);
         tailvisited.insert(tail);
     }
 }
 
-int main() {
+void move(char d, int step, std::array<std::pair<int, int>, 10>& knots, std::set<std::pair<int, int>>& tailvisited) {
+    auto f = move_func(d);
+    while (step--) {
+        f(knots[0]);
+        for (int i = 1; i < 10; i++) {
+            // if two knot is touching, the knots after that are touching, the loop can break;
+            if (check_touching_and_moving(knots[i - 1], knots[i]))
+                break;
+        }
+        tailvisited.insert(knots[9]);
+    }
+}
+
+void part1() {
     std::ifstream input("input");
 
     std::string line;
@@ -82,4 +120,29 @@ int main() {
     }
 
     std::cout << tailvisited.size() << std::endl;
+}
+
+void part2() {
+    std::ifstream input("input");
+
+    std::string line;
+
+    std::array<std::pair<int, int>, 10> knots;
+    std::set<std::pair<int, int>> tailvisited;
+    tailvisited.insert(knots[9]);
+    char direction;
+    int step;
+    while (std::getline(input, line)) {
+        direction = line[0];
+        step      = std::stoi(std::string(line.begin() + 2, line.end()));
+
+        move(direction, step, knots, tailvisited);
+    }
+
+    std::cout << tailvisited.size() << std::endl;
+}
+
+int main() {
+    part1();
+    part2();
 }
