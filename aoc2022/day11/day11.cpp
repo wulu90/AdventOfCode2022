fe8e1683569b17b6f2b269ceb73a3306e176e2f8
wulu <wulu90@outlook.com>
2022-12-11 14:56:50 +0800

    day11 part1

---
+++ aoc2022/day11/day11.cpp
@@ -0,0 +1,126 @@
+#include <deque>
+#include <fstream>
+#include <functional>
+#include <iostream>
+#include <string>
+#include <vector>
+
+std::function<int(int)> parse_operation(const std::string& line) {
+    auto old_inx = line.find("old");
+    std::function<int(int)> f;
+    int v;
+    std::string oprand = line.substr(old_inx + 6, line.size() - old_inx - 6);
+    if (line[old_inx + 4] == '*') {
+        if (oprand == "old") {
+            f = [](int p) { return p * p; };
+        } else {
+            v = std::stoi(oprand);
+            f = [v](int p) { return p * v; };
+        }
+    } else {
+        v = std::stoi(oprand);
+        f = [v](int p) { return p + v; };
+    }
+    return f;
+}
+
+int parse_test(const std::string& line) {
+    auto last_space = line.find_last_of(' ');
+    return std::stoi(line.substr(last_space + 1, line.size() - last_space - 1));
+}
+
+std::deque<int> parse_starting_items(const std::string& line) {
+    auto colon_inx = line.find(':');
+    // auto itemstr   = line.substr(colon_inx + 2, line.size() - colon_inx - 2);
+
+    size_t comma_inx;
+    auto start = colon_inx + 2;
+    std::deque<int> result;
+
+    while (start < line.size()) {
+        comma_inx = line.find(',', start);
+        if (comma_inx != std::string::npos) {
+            result.push_back(std::stoi(line.substr(start, comma_inx - start)));
+            start = comma_inx + 2;
+        } else {
+            result.push_back(std::stoi(line.substr(start, line.size() - start)));
+            break;
+        }
+    }
+
+    return result;
+}
+
+int parse_throwto(const std::string& line) {
+    auto last_space = line.find_last_of(' ');
+    return std::stoi(line.substr(last_space + 1, line.size() - last_space - 1));
+}
+
+struct Monkey {
+    std::deque<int> items;
+    std::function<int(int)> operation;
+    int testv;
+    int true_to;
+    int false_to;
+    int inspect_time = 0;
+
+    void inpect(std::vector<Monkey>& monkeys) {
+        // if (items.empty())
+        //    return;
+
+        while (!items.empty()) {
+            inspect_time += 1;
+            auto item = items.front();
+            items.pop_front();
+            auto afterop = operation(item);
+            afterop      = afterop / 3;
+            int to       = afterop % testv == 0 ? true_to : false_to;
+            monkeys[to].items.push_back(afterop);
+        }
+    }
+};
+
+Monkey parse_monkey(std::ifstream& input) {
+    Monkey m;
+    std::string line;
+    std::getline(input, line);
+    std::getline(input, line);
+    m.items = parse_starting_items(line);
+    std::getline(input, line);
+    m.operation = parse_operation(line);
+    std::getline(input, line);
+    m.testv = parse_test(line);
+    std::getline(input, line);
+    m.true_to = parse_throwto(line);
+    std::getline(input, line);
+    m.false_to = parse_throwto(line);
+    std::getline(input, line);    // read empty line for sepereate;
+    return m;
+}
+
+int main() {
+    std::ifstream input("input");
+
+    std::string line;
+    std::vector<Monkey> monkeys;
+
+    while (!input.eof()) {
+        Monkey m = parse_monkey(input);
+        monkeys.push_back(m);
+    }
+
+    for (int i = 0; i < 20; i++) {
+        for (auto& m : monkeys) {
+            m.inpect(monkeys);
+        }
+    }
+
+    std::vector<int> insptectime_vec;
+    for (auto& m : monkeys) {
+        insptectime_vec.push_back(m.inspect_time);
+    }
+
+    std::sort(insptectime_vec.begin(), insptectime_vec.end());
+
+    std::cout << *insptectime_vec.rbegin() * *(insptectime_vec.rbegin() + 1) << std::endl;
+}
