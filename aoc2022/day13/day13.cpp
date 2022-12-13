7b7ce6628f36a9c70d02ee201a6a8498f63ebbbc
wulu <wulu90@outlook.com>
2022-12-14 04:52:03 +0800

    day13 part1 std::variant

---
+++ aoc2022/day13/day13.cpp
@@ -0,0 +1,83 @@
+#include <algorithm>
+#include <fstream>
+#include <iostream>
+#include <numeric>
+#include <string>
+#include <variant>
+#include <vector>
+
+using namespace std;
+
+struct node {
+    variant<int, vector<node>> data;
+
+    explicit node(int i) : data(i) {}
+
+    explicit node(vector<node> v) : data(move(v)) {}
+
+    friend bool operator<(const node& lhs, const node& rhs) {
+        if (lhs.data.index() == 0 && rhs.data.index() == 0) {
+            return get<int>(lhs.data) < get<int>(rhs.data);
+        } else if (lhs.data.index() == 1 && rhs.data.index() == 1) {
+            return get<vector<node>>(lhs.data) < get<vector<node>>(rhs.data);
+        } else {
+            if (lhs.data.index() == 0 && rhs.data.index() == 1) {
+                return vector<node>{node{get<int>(lhs.data)}} < get<vector<node>>(rhs.data);
+            } else {
+                return get<vector<node>>(lhs.data) < vector<node>{node{get<int>(rhs.data)}};
+            }
+        }
+    }
+};
+
+int parse_int(string::iterator& s, string::iterator e) {
+    return stoi(string(s, e));
+}
+
+vector<node> parse_list(string::iterator& s, string::iterator e) {
+    vector<node> v;
+
+    s++;    // [
+    while (s != e && *s != ']') {
+        if (isdigit(*s)) {
+            auto it = find_if(s, e, [](char c) { return c == ',' || c == ']'; });
+            v.emplace_back(parse_int(s, it));
+            s = it;
+        } else if (*s == '[') {
+            v.emplace_back(parse_list(s, e));
+            s++;
+        }
+        if (*s == ',') {
+            s++;
+        }
+    }
+    return v;
+}
+
+node parse_node(string& line) {
+    auto s = line.begin();
+    auto e = line.end();
+    return node{parse_list(s, e)};
+}
+
+void part1() {
+    ifstream input("input");
+    string line;
+    vector<node> nv;
+    while (getline(input, line)) {
+        if (!line.empty())
+            nv.emplace_back(parse_node(line));
+    }
+
+    vector<size_t> right;
+    for (size_t i = 0; i < nv.size() / 2; i++) {
+        if (nv[i * 2] < nv[i * 2 + 1])
+            right.push_back(i + 1);
+    }
+
+    cout << accumulate(right.begin(), right.end(), 0) << endl;
+}
+
+int main() {
+    part1();
+}
