bd68ec930daed0d5e997fd10dc93382d52597c5d
wulu <wulu90@outlook.com>
2022-12-22 00:50:21 +0800

    day21 part2

--- aoc2022/day21/day21.cpp
+++ aoc2022/day21/day21.cpp
@@ -11,13 +11,35 @@ using namespace std;
 struct target {
     string id;
     long val;
-    function<long(long, long)> op;
+    char op;
     bool status;
     shared_ptr<target> dep1;
     shared_ptr<target> dep2;
 
     target(const string& s) : id(s) {}
 };
+
+function<long(long, long)> getfunc(char c) {
+    if (c == '+')
+        return std::plus<long>();
+    else if (c == '-')
+        return std::minus<long>();
+    else if (c == '*')
+        return std::multiplies<long>();
+    else
+        return std::divides<long>();
+}
+
+function<long(long, long)> getfunc_inverse(char c) {
+    if (c == '-')
+        return std::plus<long>();
+    else if (c == '+')
+        return std::minus<long>();
+    else if (c == '/')
+        return std::multiplies<long>();
+    else
+        return std::divides<long>();
+}
 
 void parse_target(const string& line, map<string, shared_ptr<target>>& targetmap) {
     string id = line.substr(0, 4);
@@ -41,14 +63,7 @@ void parse_target(const string& line, map<string, shared_ptr<target>>& targetmap
         t->dep1   = targetmap[d1];
         t->dep2   = targetmap[d2];
         t->status = false;
-        if (line[11] == '+')
-            t->op = std::plus<long>();
-        else if (line[11] == '-')
-            t->op = std::minus<long>();
-        else if (line[11] == '*')
-            t->op = std::multiplies<long>();
-        else if (line[11] == '/')
-            t->op = std::divides<long>();
+        t->op     = line[11];
     }
 }
 
@@ -58,9 +73,20 @@ long calc(const shared_ptr<target>& t) {
     } else {
         long l    = calc(t->dep1);
         long r    = calc(t->dep2);
-        t->val    = t->op(l, r);
+        t->val    = getfunc(t->op)(l, r);
         t->status = true;
         return t->val;
+    }
+}
+
+bool has_humn(const shared_ptr<target>& t) {
+    if (t->id == "humn")
+        return true;
+    else {
+        if (!t->status) {
+            return has_humn(t->dep1) || has_humn(t->dep2);
+        } else
+            return false;
     }
 }
 
@@ -76,6 +102,42 @@ void part1() {
     cout << num << endl;
 }
 
+void part2() {
+    ifstream input("input");
+    string line;
+    map<string, shared_ptr<target>> targetmap;
+    while (getline(input, line)) {
+        parse_target(line, targetmap);
+    }
+
+    long value = has_humn(targetmap["root"]->dep1) ? calc(targetmap["root"]->dep2) : calc(targetmap["root"]->dep1);
+
+    shared_ptr<target> curr = has_humn(targetmap["root"]->dep1) ? targetmap["root"]->dep1 : targetmap["root"]->dep2;
+
+    while (curr->id != "humn") {
+        bool left_humn = has_humn(curr->dep1) ? true : false;
+        long correct   = left_humn ? calc(curr->dep2) : calc(curr->dep1);
+
+        if (curr->op == '+' || curr->op == '*')
+            value = getfunc_inverse(curr->op)(value, correct);
+        else {
+            if (left_humn)
+                value = getfunc_inverse(curr->op)(value, correct);
+            else
+                value = getfunc(curr->op)(correct, value);
+        }
+
+        curr = left_humn ? curr->dep1 : curr->dep2;
+    }
+
+    cout << value << endl;
+
+    targetmap["humn"]->val = value;
+    cout << calc(targetmap["root"]->dep1) << endl;
+    cout << calc(targetmap["root"]->dep2) << endl;
+}
+
 int main() {
     part1();
+    part2();
 }
