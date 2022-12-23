41ef5db87e706c604f5db314a0278b58e24efe57
wulu <wulu90@outlook.com>
2022-12-23 20:23:08 +0800

    day23 part2

--- aoc2022/day23/day23.cpp
+++ aoc2022/day23/day23.cpp
@@ -157,6 +157,89 @@ void part1() {
     cout << count << endl;
 }
 
+void part2() {
+    ifstream input("input");
+    vector<string> grove;
+    string line;
+    while (getline(input, line)) {
+        grove.push_back(line);
+    }
+
+    vector<array<pair<int, int>, 3>> dirs;
+
+    dirs.push_back({{{-1, 0}, {-1, -1}, {-1, 1}}});
+    dirs.push_back({{{1, 0}, {1, -1}, {1, 1}}});
+    dirs.push_back({{{0, -1}, {-1, -1}, {1, -1}}});
+    dirs.push_back({{{0, 1}, {-1, 1}, {1, 1}}});
+
+    bool needmove = false;
+    int round     = 0;
+    while (true) {
+        needmove = false;
+        string emptyline(grove[0].size() + 2, '.');
+        vector<string> newgrove;
+        newgrove.push_back(emptyline);
+        for (auto& s : grove) {
+            string tmp(".");
+            tmp.append(s);
+            tmp.push_back('.');
+            newgrove.push_back(tmp);
+        }
+        newgrove.push_back(emptyline);
+
+        vector<pair<size_t, size_t>> from;
+        vector<vector<pair<size_t, size_t>>> r(newgrove[0].size() + 2, from);
+        vector<vector<vector<pair<size_t, size_t>>>> considers(newgrove.size() + 2, r);
+
+        for (size_t r = 0; r < newgrove.size(); r++) {
+            for (size_t c = 0; c < newgrove[0].size(); c++) {
+                if (newgrove[r][c] == '#') {
+                    auto mto = move_to(newgrove, dirs, r, c);
+                    if (mto.first != static_cast<int>(r) || mto.second != static_cast<int>(c))
+                        needmove = true;
+                    // auto [a, b] = move_to(newgrove, dirs, r, c);
+                    considers[mto.first][mto.second].push_back({r, c});
+                }
+            }
+        }
+
+        if (!needmove) {
+            break;
+        }
+
+        vector<string> emptygrove(newgrove.size(), emptyline);
+
+        for (size_t r = 0; r < newgrove.size(); r++) {
+            for (size_t c = 0; c < newgrove[0].size(); c++) {
+                if (considers[r][c].size() == 0) {
+                    continue;
+                } else if (considers[r][c].size() == 1) {
+                    emptygrove[r][c] = '#';
+                } else {
+                    for (auto [a, b] : considers[r][c]) {
+                        emptygrove[a][b] = '#';
+                    }
+                }
+            }
+        }
+
+        grove = emptygrove;
+        dirs.push_back(dirs[0]);
+        dirs.erase(dirs.begin());
+        round += 1;
+        // cout << i << endl;
+        // for (auto& s : grove) {
+        //     for (auto& c : s) {
+        //         cout << c;
+        //     }
+        //     cout << endl;
+        // }
+    }
+
+    cout << round + 1 << endl;
+}
+
 int main() {
     part1();
+    part2();
 }
