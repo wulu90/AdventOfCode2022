d5261b165616a1329f1eb7438f3898ae80181783
wulu <wulu90@outlook.com>
2022-12-25 20:34:14 +0800

    24 part1

--- aoc2022/day24/day24.cpp
+++ aoc2022/day24/day24.cpp
@@ -1,23 +1,12 @@
-#include <algorithm>
 #include <array>
+#include <chrono>
 #include <fstream>
 #include <iostream>
-#include <queue>
+#include <set>
 #include <string>
 #include <vector>
 
 using namespace std;
-
-// enum class blizzard { up, right, down, left };
-
-struct state {
-    int r;
-    int c;
-    int minutes;
-    vector<vector<vector<char>>> blimap;
-
-    state(int _r, int _c, int minu, vector<vector<vector<char>>>& blis) : r(_r), c(_c), minutes(minu), blimap(blis) {}
-};
 
 void move_blis(vector<vector<vector<char>>>& blis, vector<vector<vector<char>>>& emptyblis) {
     auto rownum = blis.size();
@@ -29,25 +18,25 @@ void move_blis(vector<vector<vector<char>>>& blis, vector<vector<vector<char>>>&
         for (size_t c = 0; c < colnum; c++) {
             for (auto& b : blis[r][c]) {
                 switch (b) {
-                case 3:
+                case 3:    // up ^
                     if (r == 0)
                         temp[rownum - 1][c].push_back(3);
                     else
                         temp[r - 1][c].push_back(3);
                     break;
-                case 0:
+                case 0:    // right >
                     if (c == colnum - 1)
                         temp[r][0].push_back(0);
                     else
                         temp[r][c + 1].push_back(0);
                     break;
-                case 1:
+                case 1:    // down v
                     if (r == rownum - 1)
                         temp[0][c].push_back(1);
                     else
                         temp[r + 1][c].push_back(1);
                     break;
-                case 2:
+                case 2:    // left <
                     if (c == 0)
                         temp[r][colnum - 1].push_back(2);
                     else
@@ -97,39 +86,35 @@ void part1() {
         count += 1;
     }
 
-    state s(0, 0, count, blis);
-
-    queue<state> qs;
-    qs.push(s);
-
     int rownum                   = blis.size();
     int colnum                   = blis[0].size();
     array<array<int, 2>, 4> dirs = {{{-1, 0}, {0, 1}, {1, 0}, {0, -1}}};
-    while (!(qs.front().r == rownum - 1 && qs.front().c == colnum - 1)) {
-        cout << qs.size() << endl;
-        auto& tmp = qs.front().blimap;
-        auto r    = qs.front().r;
-        auto c    = qs.front().c;
-        move_blis(tmp, emptyblis);
 
-        bool needwait = true;
-        for (auto [a, b] : dirs) {
-            if (r + a < 0 || r + a >= rownum || c + b < 0 || c + b >= colnum)
-                continue;
-            if (tmp[r + a][c + b].empty()) {
-                state s(r + a, c + b, qs.front().minutes + 1, tmp);
-                qs.push(s);
-                needwait = false;
+    set<pair<int, int>> positions;
+    positions.insert({0, 0});
+
+    while (!positions.contains({rownum - 1, colnum - 1})) {
+        move_blis(blis, emptyblis);
+        set<pair<int, int>> tmp_p;
+
+        for (auto& [r, c] : positions) {
+            for (auto& [a, b] : dirs) {
+                if (r + a < 0 || r + a >= rownum || c + b < 0 || c + b >= colnum)
+                    continue;
+                if (blis[r + a][c + b].empty()) {
+                    tmp_p.insert({r + a, c + b});
+                }
             }
+
+            if (blis[r][c].empty())    // wait
+                tmp_p.insert({r, c});
         }
-        if (needwait) {
-            state s(r, c, qs.front().minutes + 1, tmp);
-            qs.push(s);
-        }
-        qs.pop();
+
+        positions = tmp_p;
+        count += 1;
     }
 
-    cout << qs.front().minutes + 1 << endl;
+    cout << count + 1 << endl;
 }
 
 int main() {
