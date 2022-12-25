8698c44ddcc18755ab087cb1291dfa09ff8a7b17
wulu <wulu90@outlook.com>
2022-12-25 22:00:45 +0800

    24 part2

--- aoc2022/day24/day24.cpp
+++ aoc2022/day24/day24.cpp
@@ -8,11 +8,11 @@
 
 using namespace std;
 
-void move_blis(vector<vector<vector<char>>>& blis, vector<vector<vector<char>>>& emptyblis) {
+void move_blis(vector<vector<vector<char>>>& blis) {
     auto rownum = blis.size();
     auto colnum = blis[0].size();
-
-    auto temp = emptyblis;
+    vector<vector<char>> emptyrow(colnum);
+    vector<vector<vector<char>>> emptyblis(rownum, emptyrow);
 
     for (size_t r = 0; r < rownum; r++) {
         for (size_t c = 0; c < colnum; c++) {
@@ -20,36 +20,36 @@ void move_blis(vector<vector<vector<char>>>& blis, vector<vector<vector<char>>>&
                 switch (b) {
                 case 3:    // up ^
                     if (r == 0)
-                        temp[rownum - 1][c].push_back(3);
+                        emptyblis[rownum - 1][c].push_back(3);
                     else
-                        temp[r - 1][c].push_back(3);
+                        emptyblis[r - 1][c].push_back(3);
                     break;
                 case 0:    // right >
                     if (c == colnum - 1)
-                        temp[r][0].push_back(0);
+                        emptyblis[r][0].push_back(0);
                     else
-                        temp[r][c + 1].push_back(0);
+                        emptyblis[r][c + 1].push_back(0);
                     break;
                 case 1:    // down v
                     if (r == rownum - 1)
-                        temp[0][c].push_back(1);
+                        emptyblis[0][c].push_back(1);
                     else
-                        temp[r + 1][c].push_back(1);
+                        emptyblis[r + 1][c].push_back(1);
                     break;
                 case 2:    // left <
                     if (c == 0)
-                        temp[r][colnum - 1].push_back(2);
+                        emptyblis[r][colnum - 1].push_back(2);
                     else
-                        temp[r][c - 1].push_back(2);
+                        emptyblis[r][c - 1].push_back(2);
                     break;
                 }
             }
         }
     }
-    blis = temp;
+    blis = emptyblis;
 }
 
-void part1() {
+vector<vector<vector<char>>> parse_input() {
     ifstream input("input");
     string line;
 
@@ -75,26 +75,32 @@ void part1() {
             blis.push_back(row);
         }
     }
+    return blis;
+}
 
-    row.clear();
-    row.resize(blis[0].size());
-    vector<vector<vector<char>>> emptyblis(blis.size(), row);
-
+int wait_start_empty(vector<vector<vector<char>>>& blis, int r, int c) {
     int count = 0;
-    while (!blis[0][0].empty()) {
-        move_blis(blis, emptyblis);
+    while (!blis[r][c].empty()) {
+        move_blis(blis);
         count += 1;
     }
+    return count;
+}
+
+int step_target(vector<vector<vector<char>>>& blis, int sr, int sc, int er, int ec) {
+    int count = 0;
 
     int rownum                   = blis.size();
     int colnum                   = blis[0].size();
     array<array<int, 2>, 4> dirs = {{{-1, 0}, {0, 1}, {1, 0}, {0, -1}}};
 
     set<pair<int, int>> positions;
-    positions.insert({0, 0});
+    positions.insert({sr, sc});
 
-    while (!positions.contains({rownum - 1, colnum - 1})) {
-        move_blis(blis, emptyblis);
+    bool success = true;
+
+    while (!positions.contains({er, ec})) {
+        move_blis(blis);
         set<pair<int, int>> tmp_p;
 
         for (auto& [r, c] : positions) {
@@ -110,13 +116,61 @@ void part1() {
                 tmp_p.insert({r, c});
         }
 
+        if (tmp_p.empty()) {
+            success = false;
+            break;
+        }
+
         positions = tmp_p;
         count += 1;
     }
 
-    cout << count + 1 << endl;
+    move_blis(blis);
+    count += 1;
+
+    if (!success)
+        count = -1;
+
+    return count;
+}
+
+int go_through(vector<vector<vector<char>>>& blis, int sr, int sc, int er, int ec) {
+    auto temp = blis;
+
+    int count = 0;
+    while (true) {
+        int a = wait_start_empty(blis, sr, sc);
+        temp  = blis;
+        count += a;
+        int b = step_target(temp, sr, sc, er, ec);
+        if (b == -1) {
+            move_blis(blis);
+            count += 1;
+        } else {
+            count += b;
+            break;
+        }
+    }
+    blis = temp;
+    return count;
+}
+
+void part1() {
+    auto blis = parse_input();
+    cout << go_through(blis, 0, 0, blis.size() - 1, blis[0].size() - 1) << endl;
+}
+
+void part2() {
+    auto blis = parse_input();
+
+    auto a = go_through(blis, 0, 0, blis.size() - 1, blis[0].size() - 1);
+    auto b = go_through(blis, blis.size() - 1, blis[0].size() - 1, 0, 0);
+    auto c = go_through(blis, 0, 0, blis.size() - 1, blis[0].size() - 1);
+
+    cout << a + b + c << endl;
 }
 
 int main() {
     part1();
+    part2();
 }
