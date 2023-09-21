08373bfdcfa3f7d45f000333ea4f562ac2cac481
wulu <wulu90@outlook.com>
2023-09-21 15:44:10 +0800

    day18 part1 cube struct

--- aoc2022/day18/day18.cpp
+++ aoc2022/day18/day18.cpp
@@ -6,9 +6,21 @@
 
 using namespace std;
 
+struct cube {
+    int x;
+    int y;
+    int z;
+
+    bool operator<(const cube& r) const {
+        return tie(x, y, z) < tie(r.x, r.y, r.z);
+    }
+
+    cube(int _x, int _y, int _z) : x(_x), y(_y), z(_z) {}
+};
+
 void part1_op() {
     ifstream input("input");
-    set<tuple<int, int, int>> cubes;
+    set<cube> cubes;
     int x, y, z;
     while (input.good()) {
         input >> x;
@@ -20,14 +32,15 @@ void part1_op() {
         cubes.insert({x, y, z});
     }
 
-    auto find_neighbors = [](const tuple<int, int, int>& it) -> vector<tuple<int, int, int>> {
-        vector<tuple<int, int, int>> res;
-        res.push_back({get<0>(it) + 1, get<1>(it), get<2>(it)});
-        res.push_back({get<0>(it) - 1, get<1>(it), get<2>(it)});
-        res.push_back({get<0>(it), get<1>(it) + 1, get<2>(it)});
-        res.push_back({get<0>(it), get<1>(it) - 1, get<2>(it)});
-        res.push_back({get<0>(it), get<1>(it), get<2>(it) + 1});
-        res.push_back({get<0>(it), get<1>(it), get<2>(it) - 1});
+    auto find_neighbors = [](const cube& i) -> vector<cube> {
+        vector<cube> res;
+        res.push_back({i.x + 1, i.y, i.z});
+        res.push_back({i.x - 1, i.y, i.z});
+        res.push_back({i.x, i.y + 1, i.z});
+        res.push_back({i.x, i.y - 1, i.z});
+        res.push_back({i.x, i.y, i.z + 1});
+        res.push_back({i.x, i.y, i.z - 1});
+
         return res;
     };
 
